cb9254dc54203021d2e7d273f861652ee712e6c7
BenShepherd <19ute14@gmail.com>
2019-06-29 23:27:44 -0600

    added mongoose setup

---
+++ 07-Ins_Mern/create-react-express/client/build/precache-manifest.0870c756295ca2da9df467a1dcd7334e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7484fd2edd757fe9895a",
+    "url": "/static/js/main.f52a61bd.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "7484fd2edd757fe9895a",
+    "url": "/static/css/main.477819ef.chunk.css"
+  },
+  {
+    "revision": "c7c4e4cc3a83d4d9e9768c8e2d9b3312",
+    "url": "/index.html"
+  }
+];
